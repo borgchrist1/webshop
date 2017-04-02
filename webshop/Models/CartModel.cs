79536aa1740e1e94e48a2403f402b14b1c14f204
borgchrist1 <christian.borg@outlook.com>
2017-04-02 23:29:22 +0100

    Commit agin

--- webshop/Models/CartModel.cs
+++ webshop/Models/CartModel.cs
@@ -7,7 +7,7 @@ namespace webshop.Models
 {
     public class CartModel
     {
-       // public int Id { get; set; }
+       
         public int Merchandise_id { get; set; }
         public MerchandiseModel Product { get; set; }
         public int Count { get; set; }
